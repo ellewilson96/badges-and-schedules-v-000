5d464a4cb952c7c2a483917edfa6e067ba19bee5
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-08 16:24:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,11 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  Array.new
+def batch_badge_creator(arr)
+  new_array = []
+  arr.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+end
 end
 
 def assign_rooms
