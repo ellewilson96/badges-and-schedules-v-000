2bbbc878a5d6e0b638b8f0b5a74031e20fba2792
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-08 16:27:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,14 +19,14 @@ def assign_rooms(arr)
    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
    counter+=1
    end
-  return new_array  
+  return new_array
  end
- 
+
  def printer(arr)
    batch_badge_creator(arr).each do |badge|
      puts badge
    end
- 
+
    assign_rooms(arr).each do |assignment|
      puts assignment
    end
