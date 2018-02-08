a3870b20a0943273e9e794e57e62d6b6329e210e
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-08 16:21:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,14 @@
 # Write your code here.
 
 def badge_maker(name)
-  puts "Hello, my name is #{name}"
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+end
+
+def assign_rooms
+end
+
+def printer
 end
